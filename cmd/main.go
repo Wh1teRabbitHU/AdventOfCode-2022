d2ce917a95739d5a7710bfef3ab9c18d92d8e8d1
Tamas Ruszka <ruszka.tamas@gmail.com>
2023-12-31 12:12:53 +0100

    Day 04 - Solution 01

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,12 @@ import (
 	"day01"
 	"day02"
 	"day03"
+	"day04"
 )
 
 func main() {
 	day01.Solve()
 	day02.Solve()
 	day03.Solve()
+	day04.Solve()
 }
---
+++ pkg/solutions/day04/solution.go
@@ -0,0 +1,75 @@
+package day04
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"utils"
+)
+
+const inputPath string = "inputs/day04.txt"
+
+type AssignmentRange struct {
+	start int
+	end   int
+}
+
+func (r *AssignmentRange) fullyOverlap(other *AssignmentRange) bool {
+	if r.start >= other.start && r.end <= other.end {
+		return true
+	} else if other.start >= r.start && other.end <= r.end {
+		return true
+	}
+
+	return false
+}
+
+func resolveRanges(rangePairs []string) (AssignmentRange, AssignmentRange) {
+	firstBoundaries := strings.Split(rangePairs[0], "-")
+	firstStart, err1 := strconv.Atoi(firstBoundaries[0])
+	firstEnd, err2 := strconv.Atoi(firstBoundaries[1])
+
+	utils.HandleError(err1)
+	utils.HandleError(err2)
+
+	secondBoundaries := strings.Split(rangePairs[1], "-")
+	secondStart, err1 := strconv.Atoi(secondBoundaries[0])
+	secondEnd, err2 := strconv.Atoi(secondBoundaries[1])
+
+	utils.HandleError(err1)
+	utils.HandleError(err2)
+
+	firstRange := AssignmentRange{start: firstStart, end: firstEnd}
+	secondRange := AssignmentRange{start: secondStart, end: secondEnd}
+
+	return firstRange, secondRange
+}
+
+func Solve() {
+	inputFile, err := os.Open(inputPath)
+
+	utils.HandleError(err)
+	defer inputFile.Close()
+
+	fileScanner := bufio.NewScanner(inputFile)
+	fileScanner.Split(bufio.ScanLines)
+
+	overlapCounter := 0
+
+	for fileScanner.Scan() {
+		line := fileScanner.Text()
+		rangePairs := strings.Split(line, ",")
+		range1, range2 := resolveRanges(rangePairs)
+
+		if range1.fullyOverlap(&range2) {
+			overlapCounter++
+		}
+	}
+
+	fmt.Print("Day 04 - Solution 01: ")
+	fmt.Println(overlapCounter)
+	fmt.Print("Day 04 - Solution 02: ")
+	fmt.Println("???")
+}
