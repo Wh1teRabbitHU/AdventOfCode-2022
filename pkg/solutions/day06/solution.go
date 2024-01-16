8a401932edb3624b651946446a00e2c4ac0110e6
Tamas Ruszka <ruszka.tamas@gmail.com>
2024-01-16 21:31:13 +0100

    Day 06 - Solution 02

--- pkg/solutions/day06/solution.go
+++ pkg/solutions/day06/solution.go
@@ -10,6 +10,29 @@ import (
 
 const inputPath string = "../inputs/day06.txt"
 
+func findDistinctSection(dataStream *string, distinctLength int) int {
+	for i := distinctLength; i < len(*dataStream); i++ {
+		section := (*dataStream)[i-distinctLength : i]
+		markerFound := true
+
+		for j := 0; j < distinctLength; j++ {
+			char := section[j : j+1]
+			occurence := strings.Count(section, char)
+
+			if occurence > 1 {
+				markerFound = false
+				break
+			}
+		}
+
+		if markerFound {
+			return i
+		}
+	}
+
+	return -1
+}
+
 func Solve() {
 	inputFile, err := os.Open(inputPath)
 
@@ -21,30 +44,11 @@ func Solve() {
 	fileScanner.Scan()
 
 	dataStream := fileScanner.Text()
-	startOfPacket := 0
-
-	for i := 4; i < len(dataStream); i++ {
-		section := dataStream[i-4 : i]
-		markerFound := true
-
-		for j := 0; j < 4; j++ {
-			char := section[j : j+1]
-			occurence := strings.Count(section, char)
-
-			if occurence > 1 {
-				markerFound = false
-				break
-			}
-		}
-
-		if markerFound {
-			startOfPacket = i
-			break
-		}
-	}
+	startOfPacket := findDistinctSection(&dataStream, 4)
+	startOfMessage := findDistinctSection(&dataStream, 14)
 
 	fmt.Print("Day 06 - Solution 01: ")
 	fmt.Println(startOfPacket)
 	fmt.Print("Day 06 - Solution 02: ")
-	fmt.Println("???")
+	fmt.Println(startOfMessage)
 }
